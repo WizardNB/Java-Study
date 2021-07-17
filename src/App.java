1b28a0428f09cdeabc7475e7408308b03c3adb6a
泓昊 吴 <Minecraft00615@outlook.com>
2021-07-17 21:33:40 +0800

    master

--- src/App.java
+++ src/App.java
@@ -3,5 +3,6 @@ import study.ArrayDemo;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println(ArrayDemo.aisatsu);
+        System.out.println("tyugh");
     }
 }
